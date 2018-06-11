3d2a2b53e626557c6514ed369ce9e002b80f94a7
austin barr and Hisato Kawaminami <austin@>
2018-06-11 11:31:48 -0700

    add js code to take the input and display

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,38 @@ function Place(place, city, time, landmark, see){
   this.see = see;
 }
 
-Place.protoype.timePlace = function() {
+Place.prototype.timePlace = function() {
   return this.place + " " + this.time;
 };
 
 //user interface logic
 $(function(){
-  $("form#")
-}
+  $("form#formOne").submit(function(event){
+    event.preventDefault();
+
+    var inputtedPlace = $("input#new-place").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedTime = $("input#new-time").val();
+    var inputtedLandmark = $("input#new-landmark").val();
+    var inputtedSee = $("input#new-mustSee").val();
+
+    var newPlace = new Place(inputtedPlace, inputtedCity, inputtedTime, inputtedLandmark, inputtedSee);
+
+    $("ul#places").append("<li><span class ='places'>" + newPlace.timePlace() +"</span></li>");
+
+    $("input#new-place").val("");
+    $("input#new-city").val("");
+    $("input#new-time").val("");
+    $("input#new-landmark").val("");
+    $("input#new-mustSee").val("");
+
+    $(".places").last().click(function(){
+      $("#show-places").show();
+      $("#show-places h2").text(newPlace.place);
+      $(".city").text(newPlace.city);
+      $(".time").text(newPlace.time);
+      $(".landmark").text(newPlace.landmark);
+      $(".mustSee").text(newPlace.see);
+    });
+  });
+});
